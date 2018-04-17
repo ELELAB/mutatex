9a11149937e420f005ccf95a942009b834c732dd
Matteo Tiberti <matteo.tiberti@gmail.com>
2018-04-17 15:57:51 +0200

    added ptm support to main script

--- mutatex.py
+++ mutatex.py
@@ -28,6 +28,28 @@ import numpy as np
 import tarfile as tar
 from Bio import PDB
 
+ptm_residues = {"y": "PTR",
+                "p": "TPO",
+                "s": "SEP",
+                "h": "HYP",
+                "z": "TYS",
+                "k": "MLZ",
+                "m": "MLY",
+                "l": "M3L",
+                "o": "H1S",
+                "e": "H2S",
+                "f": "H3S"}
+
+len_three2one = len(PDB.Polypeptide.d1_to_index)
+idx=len_three2one
+for k,v in ptm_residues.iteritems():
+    PDB.Polypeptide.d1_to_index[k] = idx
+    PDB.Polypeptide.dindex_to_1[idx] = k
+
+    PDB.Polypeptide.d3_to_index[v] = idx
+    PDB.Polypeptide.dindex_to_3[idx] = v
+    idx += 1
+
 class MutationList(object):
     def __init__(self, res_groups, mutations, name="", selfmutate=False):
         self.name = name
