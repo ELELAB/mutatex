46853683d6b089b743c59ad3149acfcfa25da2ec
Matteo Tiberti <matteo.tiberti@gmail.com>
2019-06-24 14:31:56 +0200

    added mutatex package

---
+++ mutatex/__init__.py
@@ -0,0 +1,25 @@
+from Bio import PDB
+from six import iteritems
+
+ptm_residues = {"y": "PTR",
+                "p": "TPO",
+                "s": "SEP",
+                "h": "HYP",
+                "z": "TYS",
+                "k": "MLZ",
+                "m": "MLY",
+                "l": "M3L",
+                "o": "H1S",
+                "e": "H2S",
+                "f": "H3S"}
+len_three2one = len(PDB.Polypeptide.d1_to_index)
+idx=len_three2one
+for k,v in iteritems(ptm_residues):
+    PDB.Polypeptide.d1_to_index[k] = idx
+    PDB.Polypeptide.dindex_to_1[idx] = k
+
+    PDB.Polypeptide.d3_to_index[v] = idx
+    PDB.Polypeptide.dindex_to_3[idx] = v
+    idx += 1
+
+import utils
