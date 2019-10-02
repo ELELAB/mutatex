c1f9b4d016b94bcc6b953414151673c2e162fa58
Matteo Tiberti <matteo.tiberti@gmail.com>
2019-10-02 12:01:16 +0200

    added p53 example

---
+++ examples/p53/run.sh
@@ -0,0 +1,12 @@
+unset $PYTHONPATH
+. /home/teo/envs/mutatex/bin/activate
+
+mutatex 3kmd.pdb \
+	-p 4 \
+	-m mutation_list.txt \
+	-x /usr/local/foldx5/foldx \
+	-f suite5 \
+	-R ../../templates/foldxsuite4/repair_runfile_template.txt \
+	-M ../../templates/foldxsuite4/mutate_runfile_template.txt \
+	-I ../../templates/foldxsuite4/interface_runfile_template.txt \
+	-B
