ae8fb75e7be4ed383ce0aa673861327eff65dd29
Matteo Tiberti <matteo.tiberti@gmail.com>
2022-01-28 12:50:47 +0100

    added files for SH2 example

--- examples/SH2/CRK/run.sh
+++ examples/SH2/CRK/run.sh
@@ -2,9 +2,12 @@
 mutatex 1ju5.pdb \
 	-p 4 \
 	-m mutation_list.txt \
-	-x /usr/local/foldx5/foldx \
-	-f suite5 \
-	-R ../../../templates/foldxsuite4/repair_runfile_template.txt \
-	-M ../../../templates/foldxsuite4/mutate_runfile_template.txt \
-	-I ../../../templates/foldxsuite4/interface_runfile_template.txt \
+	-x /usr/local/foldx4/foldx \
+	--rotabase /usr/local/foldx4/rotabase.txt \
+	-f suite4 \
+	-R repair_runfile_template.txt \
+	-M mutate_runfile_template.txt \
+	-I interface_runfile_template.txt \
 	-B
+
+rm -rf mutations
