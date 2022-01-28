beb4f5e4f887aafb77b2c49e1031ff8c9fb2468a
Matteo Tiberti <matteo.tiberti@gmail.com>
2022-01-28 10:56:25 +0100

    modified run file to work on the correct file names

--- examples/PTEN/run.sh
+++ examples/PTEN/run.sh
@@ -1,8 +1,18 @@
-mutatex 1D5R_clean.pdb \
+mutatex 1D5R_noheatm.pdb \
 	-p 4 \
 	-m mutation_list.txt \
 	-x /usr/local/foldx5/foldx \
 	-f suite5 \
-	-R ../../templates/foldxsuite4/repair_runfile_template.txt \
-	-M ../../templates/foldxsuite4/mutate_runfile_template.txt
+	-R repair_runfile_template.txt \
+	-M mutate_runfile_template.txt \
+
+# manual cleaning for this run
+cd mutations
+find . -name runfile.txt | xargs rm
+find . -name individual_list.txt | xargs rm
+find . -name rotabase.txt | xargs rm
+find . -name '*.pdb' | xargs rm
+cd ..
+tar cvzf mutations.tar.gz mutations
+rm -rf mutations
 	
