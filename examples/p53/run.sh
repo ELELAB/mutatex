868cb27f1ba7231d4424048906fec0e3b8eda352
Matteo Tiberti <matteo.tiberti@gmail.com>
2022-01-28 12:17:50 +0100

    added results for p53 runs

--- examples/p53/run.sh
+++ examples/p53/run.sh
@@ -1,12 +1,18 @@
-unset $PYTHONPATH
-. /home/teo/envs/mutatex/bin/activate
+# notice that this was run with a previous version of mutatex, 
+# the --skip-check option is not available anymore. Just remove
+# it to rerun the scan with a more modern version
 
 mutatex 3kmd.pdb \
 	-p 4 \
 	-m mutation_list.txt \
-	-x /usr/local/foldx5/foldx \
-	-f suite5 \
-	-R ../../templates/foldxsuite4/repair_runfile_template.txt \
-	-M ../../templates/foldxsuite4/mutate_runfile_template.txt \
-	-I ../../templates/foldxsuite4/interface_runfile_template.txt \
-	-B
+	-x /usr/local/foldx4/foldx \
+	-f suite4 \
+	--rotabase /usr/local/foldx4/rotabase.txt \
+	-R repair_runfile_template.txt \
+	-M mutate_runfile_template.txt \
+	-I interface_runfile_template.txt \
+	--clean-deep \
+	--binding-energy \
+	--skip-check \
+	--foldx-log
+
