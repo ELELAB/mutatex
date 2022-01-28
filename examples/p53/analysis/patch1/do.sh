df2b528863f793fe8f347937625e1edf6ea02a2f
Matteo Tiberti <matteo.tiberti@gmail.com>
2022-01-28 12:19:31 +0100

    updated analysis scripts

--- examples/p53/analysis/patch1/do.sh
+++ examples/p53/analysis/patch1/do.sh
@@ -1,7 +1,7 @@
 export BASEDIR=../../
 
 export MUTLIST=${BASEDIR}/mutation_list.txt
-export RESULTS=${BASEDIR}/results/interface_ddgs/3kmd_model0_checked_Repair/A-B/
+export RESULTS=${BASEDIR}/results/interface_ddgs/3kmd_model0_Repair/A-B/
 export PDB=./3kmd_173-184_238-247.pdb
 
 ddg2heatmap -p $PDB -d $RESULTS -l $MUTLIST -s 50 -F Arial -n -3 -x 5 -c plasma -b labels.csv 
