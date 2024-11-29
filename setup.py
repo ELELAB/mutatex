b7923be92198e41872f50dfda6f05aca35e0f166
Mattia Utichi <mattia@kb-bioinfo01.cancer.dk>
2024-11-29 17:30:37 +0100

    removed tabs in setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
 
     install_requires=[ 'biopython',
                        'matplotlib',
-		               'numpy',
+                       'numpy',
                        'pandas',
                        'logomaker',
                        'scipy',
